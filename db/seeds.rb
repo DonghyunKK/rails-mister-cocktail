3924704233c96b459287fd37eca0b3701b2340ca
donghyun kim <donghyunkim524@gmail.com>
2020-11-12 17:06:46 +0000

    generate views and controllers and works fine

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# require 'open-uri'
+# require 'json'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -9,3 +11,17 @@
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+
+# Ingredient.delete_all
+
+# open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list") do |ingredients|
+#   data = []
+#   ingredients.read.each_line do |ingredient|
+#     @item = JSON.parse(ingredient)
+#       object = {
+#     		"name":     @item["file_url"]
+#       }
+#       data << object
+#   end
+#   Ingredient.create!(data)
+# end
